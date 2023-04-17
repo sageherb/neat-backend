c8f5eb84d9fafab8a2fd8c8d235ea1648b1c06cb
xollee <devleexol@gmail.com>
2023-04-17 12:31:36 +0900

    build: 프로젝트 생성
    - 프로젝트 생성
    - ESLint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "no-unused-vars": "warn",
+  },
+};
