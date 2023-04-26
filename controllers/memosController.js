a8ad5d37c4a5f0e7c8e7b4fa687365aa1c2aa021
xollee <devleexol@gmail.com>
2023-04-26 18:51:06 +0900

    feat: getMemo 구현

--- controllers/memosController.js
+++ controllers/memosController.js
@@ -4,12 +4,13 @@ const getAllMemos = async (req, res, next) => {
   try {
     const { user } = req;
     const offset = parseInt(req.query.offset, 10) || 0;
-    const limit = parseInt(req.query.limit, 10) || 10;
+    const limit = parseInt(req.query.limit, 10) || 20;
 
     const memos = await Memo.find({ owner: user })
       .sort({ createdAt: -1 })
       .skip(offset)
-      .limit(limit);
+      .limit(limit)
+      .lean();
 
     res.status(200).json({ memos });
   } catch (error) {
@@ -20,13 +21,26 @@ const getAllMemos = async (req, res, next) => {
 const createMemo = async (req, res, next) => {
   try {
     const { user } = req;
-    const newMemo = new Memo({
+    const memo = new Memo({
       owner: user,
+      content: "",
     });
 
-    await Memo.create(newMemo);
+    await Memo.create(memo);
 
-    res.status(201).json({ memoId: newMemo._id });
+    res.status(201).json({ memo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMemo = async (req, res, next) => {
+  try {
+    const { memoId } = req.params;
+
+    const memo = await Memo.findById(memoId).lean();
+
+    res.status(200).json({ memo });
   } catch (error) {
     next(error);
   }
@@ -37,12 +51,16 @@ const updateMemo = async (req, res, next) => {
     const { memoId } = req.params;
     const { content } = req.body;
 
-    await Memo.findByIdAndUpdate(memoId, { content });
+    const memo = await Memo.findByIdAndUpdate(
+      memoId,
+      { content },
+      { returnDocument: "after" }
+    ).lean();
 
-    res.status(200).json({ memoId });
+    res.status(200).json({ memo });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getAllMemos, createMemo, updateMemo };
+module.exports = { getAllMemos, createMemo, getMemo, updateMemo };
--- routes/api/memos.js
+++ routes/api/memos.js
@@ -7,7 +7,7 @@ const authenticate = require("../../middlewares/authenticate");
 
 router.get("/", authenticate, memosController.getAllMemos);
 router.post("/", authenticate, memosController.createMemo);
-// router.get("/:memoId", authenticate, memosController.getMemo);
+router.get("/:memoId", authenticate, memosController.getMemo);
 router.put("/:memoId", authenticate, memosController.updateMemo);
 // router.delete("/:memoId", authenticate, memosController.deleteMemo);
 
