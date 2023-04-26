3b3dbedfb331ce68fbfcb2c2a76da0122f81b6c8
xollee <devleexol@gmail.com>
2023-04-26 16:38:15 +0900

    feat: getAllMemos 구현

--- controllers/memosController.js
+++ controllers/memosController.js
@@ -1,5 +1,22 @@
 const Memo = require("../models/Memo");
 
+const getAllMemos = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const offset = parseInt(req.query.offset, 10) || 0;
+    const limit = parseInt(req.query.limit, 10) || 10;
+
+    const memos = await Memo.find({ owner: user })
+      .sort({ createdAt: -1 })
+      .skip(offset)
+      .limit(limit);
+
+    res.status(200).json({ memos });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createMemo = async (req, res, next) => {
   try {
     const { user } = req;
@@ -28,4 +45,4 @@ const updateMemo = async (req, res, next) => {
   }
 };
 
-module.exports = { createMemo, updateMemo };
+module.exports = { getAllMemos, createMemo, updateMemo };
--- routes/api/memos.js
+++ routes/api/memos.js
@@ -5,7 +5,7 @@ const router = express.Router({ mergeParams: true });
 const memosController = require("../../controllers/memosController");
 const authenticate = require("../../middlewares/authenticate");
 
-// router.get("/", authenticate, memosController.getAllMemos);
+router.get("/", authenticate, memosController.getAllMemos);
 router.post("/", authenticate, memosController.createMemo);
 // router.get("/:memoId", authenticate, memosController.getMemo);
 router.put("/:memoId", authenticate, memosController.updateMemo);
